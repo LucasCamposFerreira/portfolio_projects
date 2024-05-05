4f6874e6d169012a5a19b8a3b1e74850d0b9d01f
Lucas Campos Ferreira <66262807+LucasCamposFerreira@users.noreply.github.com>
2024-05-05 17:19:39 -0300

    update archives

---
+++ src/components/theHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <header class="headPag">
+    <div id="titlePag">
+      <h1 id="namePag">Portfólio - Lucas Campos</h1>
+    </div>
+    <nav>
+      <div>
+        <a
+          class="icoCntt" id="github"
+          href="https://github.com/LucasCamposFerreira?tab=repositories"
+          target="_blank"
+          ><img src="../assets/GitHub-Mark-32px.png" alt="img github"
+        /></a>
+        <a
+          class="icoCntt"
+          href="https://instagram.com/ilucascmp"
+          target="_blank"
+          ><img src="../assets/instagram.png" alt="img instagram"
+        /></a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Staatliches&display=swap');
+
+.headPag {
+  background: #bf172a;
+  padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  box-shadow: 0 0 1.5em #2c2c2c70;
+  margin-bottom: 50px;
+}
+.icoCntt {
+  padding: 15px 13px 0 16px;
+}
+#github {
+	border-right: 2px solid black;
+}
+header {
+  position: relative;
+}
+#titlePag #namePag {
+  position: absolute;
+  top: 40%;
+  color: black;
+  font-family: "Staatliches", cursive; 
+  font-size: 20px;
+}
+</style>
---
+++ src/components/theMain.vue
@@ -0,0 +1,51 @@
+<template>
+  <main>
+    <section id="reps">
+      <div class="modal1">
+        <div class="modal-wrapper">
+          <h3 class="titleModal">Wise Goat</h3>
+          <img src="../assets/bodezinho.png" alt="bode" class="Imgs" />
+          <p><a href="https://github.com/LucasCamposFerreira/WiseGoat" target="_blank">Clique aqui para acessá-la ></a></p>
+        </div>
+      </div>
+      <div class="modal2">
+        <div class="modal-wrapper">
+          <h3 class="titleModal">Netflix - Login</h3>
+          <img src="../assets/netflix.png" alt="netflix" class="Imgs" />
+          <p><a href="https://github.com/LucasCamposFerreira/pag-netflix">Clique aqui para acessá-la ></a></p>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style>
+.modal1, .modal2 {
+  width: 60%;
+  display: flex;
+  justify-content: center;
+  align-content: space-around;
+}
+.modal-wrapper {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 50px 100px;
+  border: 5px solid none;
+  border-radius: 40px;
+  box-shadow: 0 0 1em #2c2c2c70;
+}
+.Imgs {
+  align-self: center;
+  width: 200px;
+  padding: 10px 0 10px 0;
+}
+.titleModal {
+  align-self: center;
+  padding: 10px 0 10px 0;
+}
+#reps {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
