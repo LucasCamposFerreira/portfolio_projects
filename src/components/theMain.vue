9fd867813669bdfa8ac5d6615e77083e88f3077b
Lucas Campos Ferreira <66262807+LucasCamposFerreira@users.noreply.github.com>
2024-05-05 17:48:23 -0300

    Update theMain.vue

--- src/components/theMain.vue
+++ src/components/theMain.vue
@@ -5,7 +5,7 @@
         <div class="modal-wrapper">
           <h3 class="titleModal">Wise Goat</h3>
           <img src="../assets/bodezinho.png" alt="bode" class="Imgs" />
-          <p><a href="https://github.com/LucasCamposFerreira/WiseGoat" target="_blank">Clique aqui para ver o repositório ></a></p>
+          <p><a href="https://github.com/LucasCamposFerreira/wise-goat" target="_blank">Clique aqui para ver o repositório ></a></p>
           <p><a href="https://wise-goat-six.vercel.app/" target="_blank">Clique aqui para ver funcionando ></a></p>
         </div>
       </div>
